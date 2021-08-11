fe288d455ce443ae12dca130139d7356ed2aeecd
Erick <erisrnb18@gmail.com>
2021-08-11 16:09:19 -0400

    routes for airline

--- app/application.rb
+++ app/application.rb
@@ -8,20 +8,50 @@ class Application
 #airline routes
 
   #airline index
-if req.path.match('/airlines') && req.get?
+if req.path == ('/airlines') && req.get?
   return [200, {'Content-type' => 'application/json'}, [Airline.all.to_json]]
 end
 
 
   #airline create
+  if req.path == ('/airlines') && req.post?
+    body = JSON.parse(req.body.read)
+    airline = Airline.create(body)
+    return [201, {'Content-Type' => 'application/json'}, [airline.to_json]]
+  end
 
   #airline show
+  if req.path.match('/airlines/') && req.get?
+    id = req.path.split('/')[2]
+    begin
+      airline = Airline.find(id)
+      return [200, {'Content-Type' => 'application/json'}, [airline.to_json]]
+    rescue
+      return [404, {'Content-Type' => 'application/json'}, [{message: "Airline not found"}.to_json]]
+    end
+  end
 
   #airline update
+  if req.path.match('/airlines/') && req.patch?
+    id = req.path.split('/')[2]
+    body = JSON.parse(req.body.read)
+    begin
+      airline = Airline.find(id)
+      airline.update(body)
+      return [202, {'Content-Type' => 'application/json'}, [airline.to_json]]
 
   #airline delete 
 
-
+  if req.path.match('/airlines/') && req.delete?
+    id = req.path.split('/')[2]
+    begin
+      airline = Airline.find(id)
+      airline.destroy
+      return [200, {'Content-Type' => 'application/json'}, [{message: "Airline Destroyed"}.to_json]]
+    rescue
+      return [404, {'Content-Type' => 'application/json'}, [{message: "Airline not found"}.to_json]]
+    end
+  end
 
 
 
