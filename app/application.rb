af911380dfc58099cf4d58f0256b23e6befcbee9
Erick <erisrnb18@gmail.com>
2021-08-11 16:36:02 -0400

    routes for review

--- app/application.rb
+++ app/application.rb
@@ -54,10 +54,57 @@ end
     end
   end
 
+  #review routes
 
+  #review index
 
+  if req.path == '/reviews' && req.get?
+    return [200, {'Content-Type' => 'application/json'}, [Review.all.to_json]]
+  end
 
+#review create
+if req.path == ('/reviews') && req.post?
+  body = JSON.parse(req.body.read)
+  review = Review.create(body)
+  return [201, {'Content-Type' => 'application/json'}, [review.to_json]]
+end
 
+#review show
+
+if req.path.match('/reviews/') && req.get?
+  id = req.path.split('/')[2]
+  begin
+    review = Review.find(id)
+    return [200, {'Content-Type' => 'application/json'}, [review.as_json(include: :airlines).to_json]]
+  rescue
+    return [404, {'Content-Type' => 'application/json'}, [{message: "Airline not found"}.to_json]]
+  end
+end
+
+#review update
+
+if req.path.match('/reviews/') && req.patch?
+  id = req.path.split('/')[2]
+  body = JSON.parse(req.body.read)
+  begin
+    review = Review.find(id)
+    review.update(body)
+    return [202, {'Content-Type' => 'application/json'}, [review.to_json]]
+  end
+end
+
+#review delete
+
+if req.path.match('/reviews/') && req.delete?
+  id = req.path.split('/')[2]
+  begin
+    review = Review.find(id)
+    review.destroy
+    return [200, {'Content-Type' => 'application/json'}, [{message: "Review Destroyed"}.to_json]]
+  rescue
+    return [404, {'Content-Type' => 'application/json'}, [{message: "Review not found"}.to_json]]
+  end
+end
 
 
     #test route
